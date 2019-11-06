578929e4ee71a8e02d4697fd2afca518ea0ac6cf
chennatiru <chenna.kesava@iitgn.ac.in>
2019-11-06 21:02:17 +0530

    second commit

---
+++ helpers/threads.py
@@ -0,0 +1,47 @@
+import psutil
+import json
+import sys
+
+final_dict = {
+    "children":{},
+    "threads":{}
+}
+
+a = psutil.Process(int(sys.argv[1]))
+
+try: 
+    if a.threads() != []:
+        t_arr = []
+        for threads in a.threads():
+            t_arr.append(list(threads))
+        
+            # print(threads) 
+        # print(t_arr)
+        final_dict["threads"]["output"] = t_arr
+    else:
+        # print("No threads are running")
+        final_dict["threads"]["error"] = "No threads running under this process"
+except:
+    # print("Access Denied")
+    final_dict["threads"]["access_denied_error"] = "Access Denied"
+try:
+    if a.children() != []:
+        c_arr = []
+        for child in a.children():
+            # print(child)
+            td = child.as_dict(attrs = ['name', 'pid', 'ppid', 'username', 'cpu_percent', 'status', 'memory_percent','num_threads'])
+            td['cpu_percent'] = round(td['cpu_percent'], 2)
+            td['memory_percent'] = round(td['memory_percent'], 2)
+            c_arr.append(td)
+            # c_arr.append(child)
+        # print(c_arr)
+        final_dict["children"]["output"] = c_arr
+    else:
+        # print("No children process are running")
+        final_dict["children"]["error"] = "No child process running"
+except:
+    # print("Access Denied")
+    final_dict["children"]["access_denied_error"] = "Access Denied"
+
+# print(final_dict)
+print(json.dumps(final_dict))
--- scripts/expand_processes.js
+++ scripts/expand_processes.js
@@ -15,11 +15,15 @@ const electron = require('electron');
         console.log("I'm also working");
         
         ipcRenderer.once('expand_flag',function(e,item){
-            const python_s = require('child_process').spawn('python', ['./helpers/show_threads.py',item]);
+            const python_s = require('child_process').spawn('python', ['./helpers/threads.py',item]);
             console.log(item);
             var text = `Threads for process with pid  <mark>${item}</mark>`;
-            document.getElementById("head").innerHTML = text;
-            
+            document.getElementById("threads_head").innerHTML = text;
+
+            var text = `Children for process with pid  <mark>${item}</mark>`;
+            document.getElementById("children_head").innerHTML = text;
+
+
             python_s.stdout.on('data',function(out_put){
                 console.log("hii")
                 var ans = JSON.parse(out_put.toString('utf8'));
@@ -32,7 +36,7 @@ const electron = require('electron');
                     // document.getElementById("error_alert").hide()
                     if(ans["threads"]["access_denied_error"]){
                         console.log(ans["threads"]["access_denied_error"])
-                        document.getElementById("access_denied_alert").style.display = "block"
+                        document.getElementById("threads_access_denied_alert").style.display = "block"
                     }
                     
                     if(ans["threads"]["error"]){
@@ -48,6 +52,37 @@ const electron = require('electron');
                         }
                         
                         document.getElementById("threads_table_body").appendChild(row);
+                        
+                        
+                    }
+
+                    }
+
+                    if(ans["children"]["access_denied_error"]){
+                        console.log(ans["children"]["access_denied_error"])
+                        document.getElementById("childern_access_denied_alert").style.display = "block"
+                    }
+                    
+                    if(ans["children"]["error"]){
+                        console.log(ans["children"]["error"])
+                        document.getElementById("children_none_alert").style.display = "block"
+                    }else{
+
+                        for (let i = 0; i < ans["children"]["output"].length; i++) {
+                        var row = document.createElement("tr");
+                        
+                        makecol(row,ans["children"]["output"][i]["name"]);
+                        makecol(row,ans["children"]["output"][i]["pid"]);
+                        makecol(row,ans["children"]["output"][i]["ppid"]);
+                        makecol(row,ans["children"]["output"][i]["num_threads"]);
+                        makecol(row,ans["children"]["output"][i]["username"]);
+                        makecol(row,ans["children"]["output"][i]["memory_percent"]);
+                        makecol(row,ans["children"]["output"][i]["cpu_percent"]);
+                        makecol(row,ans["children"]["output"][i]["status"]); 
+                        
+                        
+                        
+                        document.getElementById("children_table_body").appendChild(row);
                         
                         
                     }
